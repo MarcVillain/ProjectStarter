be747a373d8b76de64e5d0e16cae1f49fdb8b329
Marc Villain <marc.villain@epita.fr>
2021-01-31 10:55:22 +0100

    fix(project): template's dependencies not properly installed

--- projectstarter/config.py
+++ projectstarter/config.py
@@ -1,9 +1,4 @@
-import os
-
-from pkg_resources import resource_filename, Requirement
-
-# Root directory (based on where the present file was installed)
-_ROOT = os.path.abspath(os.path.dirname(__file__))
+import pkg_resources
 
 # Path to the templates folder
-templates_folder = os.path.join(_ROOT, "templates")
+templates_folder = pkg_resources.resource_filename('projectstarter', 'templates')
